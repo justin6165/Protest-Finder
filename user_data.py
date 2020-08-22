77473d7ba0affd5da95b0fe9190680f6cec04d67
lijustin <lijustin@umich.edu>
2020-08-21 23:17:05 -0500

    added user_data.py that records saved protests

--- user_data.py
+++ user_data.py
@@ -4,6 +4,8 @@ import notification
 # event list in saved_protests.dat will be tuples in form of:
 # (title, date, link)
 
+# executes on start-up
+# checks to see if there are reminders for today
 def check_reminders():
     events = pickle.load(open("saved_protests.dat", "rb"))
 
@@ -12,3 +14,24 @@ def check_reminders():
             if (notification.check_alert(event[1])):
                 notification.send_notification(event[0], event[2])
                 events.remove(event)
+
+# saves protest information into pre-existing list
+def add_protest(title, date, link):
+    events = pickle.load(open("saved_protests.dat", "rb"))
+    protest_event = title, date, link
+    events.append(protest_event)
+    pickle.dump(events, open("saved_protests.dat", "wb"))
+
+# returns a list containing the User's saved protests
+def get_saved_protest_list():
+    events = pickle.load(open("saved_protests.dat", "rb"))
+    return events
+
+# deletes a protest from the saved protest list given the protest title
+def delete_protest(title):
+    events = pickle.load(open("saved_protests.dat", "rb"))
+    for event in events:
+        if event[0] == title:
+            events.remove(event)
+
+check_reminders()
