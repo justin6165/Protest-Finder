3ab86761d0ce651b116e5dbdb86afc94e80cf1d3
Alessandro <alessandrosnyder1999@gmail.com>
2020-08-23 00:30:27 -0500

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -65,13 +65,17 @@ def add_protest(protest, layout):
     except:
         events = []
         pickle.dump(events, open("saved_protests.dat", "wb"))
-        
+
+    exists = False
     for event in events:
-        if protest.title != event.title:
-            events.append(protest)
-            pickle.dump(events, open("saved_protests.dat", "wb"))
-            notice_dialog.NoticeDialog("Saved!", False)
+        if protest.title == event.title:
+            exists = True
             break
+
+    if not exists:
+        events.append(protest)
+        pickle.dump(events, open("saved_protests.dat", "wb"))
+        notice_dialog.NoticeDialog("Saved!", False)
 
     gui.Window.load_saves(layout)
 
@@ -109,7 +113,6 @@ def add_home_address(address):
         pickle.dump(address, open("user_address.dat", "wb"))
 
     update_protests_list()
-    notice_dialog.NoticeDialog("Saved!", False)
 
 
 def check_home_address():
